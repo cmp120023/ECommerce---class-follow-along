24ac39b8b236fbfc45b2bc5210b932c95ada2812
cody pekarek <codym.pekarek@hotmail.com>
2026-08-31 12:51:59 -0700

    Added product controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Ecommerce.Data;
+using Ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    
+    public class ProductController : Controller
+    {
+        private readonly ProductDbContext _context;
+
+        public ProductController(ProductDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(p);
+                await _context.SaveChangesAsync(); 
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(p);
+        }
+    }
+}

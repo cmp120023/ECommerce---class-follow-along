b1cc5360c7180914217c25e3bb0512759b644e40
cody pekarek <codym.pekarek@hotmail.com>
2026-08-31 11:55:59 -0700

    Add product Db context

---
+++ Data/ProductDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+
+        }
+
+        //tracked entities
+        public DbSet<Product> Products { get; set; }
+    }
+}

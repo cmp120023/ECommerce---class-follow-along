f7f28ad8eccd6190400fc4871cb226e32c950561
cody pekarek <codym.pekarek@hotmail.com>
2026-08-31 11:36:33 -0700

    Created the Product class

---
+++ Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Models
+{
+    public class Product
+    {
+        /// <summary>
+        /// Product Identifier
+        /// </summary>
+        [Key]
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// The user facing title of the product
+        /// </summary>
+        [Required]
+        [StringLength(50,ErrorMessage ="Title can not be more than 50 characters.")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// current5 sales price of product
+        /// </summary>
+        [Range(0,10_000)]
+        public double Price { get; set; }
+    }
+}

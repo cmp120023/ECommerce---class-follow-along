ec67a44dbccda7e7134c087f6be51d47109469b7
cody pekarek <codym.pekarek@hotmail.com>
2026-08-31 19:34:51 -0700

    Add member entity with migration and model updates

---
+++ Models/Member.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Models
+{
+    public class Member
+    {
+        [Key]
+        public int MemberId { get; set; }
+
+        [RegularExpression("^[a-zA-Z0-9]+$",
+            ErrorMessage = "user must be alphnumeric")]
+        [StringLength(25)]
+        public required string Username { get; set; }
+
+        public required string Email { get; set; }
+
+        [StringLength(50, MinimumLength = 6, 
+            ErrorMessage ="your password must between 6 and 50 characters long")]
+        public required string Password { get; set; }
+
+        public DateOnly DateOfBirth { get; set; }
+
+    }
+}

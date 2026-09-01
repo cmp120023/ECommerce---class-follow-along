8161249bae01693aec10804e6535c06299bb8c2b
cody pekarek <codym.pekarek@hotmail.com>
2026-08-31 20:27:18 -0700

    Update Member Controller for Registration handling

--- Controllers/MemberController.cs
+++ Controllers/MemberController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ecommerce.Data;
+using Ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerce.Controllers
 {
     public class MemberController : Controller
     {
-       public IActionResult Register()
+        private readonly ProductDbContext _context;
+
+        public MemberController(ProductDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationViewModel reg)
+        {
+            if (ModelState.IsValid)
+            {
+                Member newMember = new()
+                {
+                    Username = reg.Username,
+                    Email = reg.Email,
+                    Password = reg.Password,
+                    DateOfBirth = reg.DateOfBirth,
+                };
+                _context.Members.Add(newMember);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(reg);
         }
     }
 }
